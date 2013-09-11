600f8e36731a658a4b2af59bb9420ae79394fb87
Enrique da Costa Cambio <enrique.dacostacambio@gmail.com>
2013-09-10 23:59:35 -0700

    Examples project build fixed

--- iotake-sullerj-examples/src/main/java/com/iotake/suller/sullerj/examples/basic/BasicExample.java
+++ iotake-sullerj-examples/src/main/java/com/iotake/suller/sullerj/examples/basic/BasicExample.java
@@ -1,18 +1,13 @@
 package com.iotake.suller.sullerj.examples.basic;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.lang.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
-import org.hamcrest.CoreMatchers;
 
 import com.iotake.suller.sullerj.binder.ExtendedDocumentObjectBinder;
 import com.iotake.suller.sullerj.binder.ExtendedDocumentObjectBinderBuilder;
@@ -102,23 +97,15 @@ public class BasicExample {
     SolrQuery query = new SolrQuery();
     query.setQuery("Person__id:" + user.id);
     QueryResponse rsp = server.query(query);
-    List<Object> storedUsers = rsp.getBeans(Object.class);
+    List<Object> storedEntities = rsp.getBeans(Object.class);
 
     // some assertions
-    assertEquals(1, storedUsers.size());
-    Object firstEntity = storedUsers.get(0);
-    System.out.println("\nRetrieved:\n"
-        + ReflectionToStringBuilder.toString(firstEntity,
-            ToStringStyle.MULTI_LINE_STYLE));
-    assertThat(firstEntity, CoreMatchers.instanceOf(User.class));
-    User storedUser = (User) firstEntity;
-    assertEquals(user.id, storedUser.id);
-    assertEquals(user.name, storedUser.name);
-    assertArrayEquals(user.aliases, storedUser.aliases);
-    assertEquals(user.email, storedUser.email);
-    assertEquals(user.phones.size(), storedUser.phones.size());
-    for (int i = 0; i < user.phones.size(); i++) {
-      assertEquals(user.phones.get(i).name, storedUser.phones.get(i).name);
+    System.out.println("Result count: " + storedEntities.size());
+    for (Object storedEntity : storedEntities) {
+      System.out.println("\nRetrieved:\n"
+          + ReflectionToStringBuilder.toString(storedEntity,
+              ToStringStyle.MULTI_LINE_STYLE));
     }
+
   }
 }
